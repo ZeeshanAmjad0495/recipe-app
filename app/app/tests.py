6bbb9394ceef8826ce4323b8029fe4ee7d4d0829
ZeeshanAmjad0495 <zeeshan04956@gmail.com>
2022-07-28 19:02:25 +0500

    Fix: added wait_for_db command

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,16 +1,24 @@
 """
 Sample tests
 """
-from django.test import SimpleTestCase
+from unittest.mock import patch
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import TestCase
 
-from app import calc
 
+class CommandTests(TestCase):
+    def test_wait_for_db_ready(self):
+        """ Test waiting for db when db is available """
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as getitem:
+            getitem.return_value = True
+            call_command('wait_for_db')
+            self.assertEqual(getitem.call_count, 1)
 
-class CalcTests(SimpleTestCase):
-    """Test the calc module"""
-
-    def test_add_numbers(self):
-        """Test adding numbers together."""
-        res = calc.add(5, 6)
-
-        self.assertEqual(res, 11)
+    @patch('time.sleep', return_value=True)
+    def test_wait_for_db(self, ts):
+        """ Test waiting for db."""
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as getitem:
+            getitem.side_effect = [OperationalError] * 5 + [True]
+            call_command('wait_for_db')
+            self.assertEqual(getitem.call_count, 6)
